264a3644195a34e2446c2eddb1905bad4e1be6bb
miguecrea <127994317+miguecrea@users.noreply.github.com>
2025-07-23 14:08:50 +0200

    Added Singleton Code
    Singleton pattern to only have one intance of the class, from gamem programming patterns

---
+++ src/utility/singleton.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+namespace dae
+{
+    template <typename T>
+    class singleton
+    {
+    public:
+        static T &instance()
+        {
+            static T instance{};
+            return instance;
+        }
+
+        virtual ~singleton() = default;
+        
+        singleton(singleton const &other)            = delete;
+        singleton(singleton &&other)                 = delete;
+        singleton &operator=(singleton const &other) = delete;
+        singleton &operator=(singleton &&other)      = delete;
+
+    protected:
+        singleton() = default;
+    };
+}
