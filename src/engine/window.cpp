ca5ce5a6ad8915adb781c7bdcdde51b2be5ab59d
miguecrea <127994317+miguecrea@users.noreply.github.com>
2025-07-25 12:53:54 +0200

    Added Window
    Added Code for initalization of window

---
+++ src/engine/window.cpp
@@ -0,0 +1,54 @@
+﻿#include "window.h"
+
+// Standard includes
+#include <stdexcept>
+
+namespace dae
+{
+    window::~window()
+    {
+        glfwDestroyWindow(window_ptr_);
+        glfwTerminate();
+    }
+
+    void window::create_window_surface(VkInstance instance, VkSurfaceKHR *surface_ptr)
+    {
+        if (glfwCreateWindowSurface(instance, window_ptr_, nullptr, surface_ptr) != VK_SUCCESS)
+        {
+            throw std::runtime_error{"Failed to create window surface!"};
+        }
+    }
+
+    void window::framebuffer_resize_callback(GLFWwindow *window_ptr, int width, int height)
+    {
+        auto updated_window = reinterpret_cast<window*>(glfwGetWindowUserPointer(window_ptr));
+        updated_window->frame_buffer_resized_ = true;
+        updated_window->width_ = width;
+        updated_window->height_ = height;
+    }
+
+    void window::init(int width, int height, std::string const &name)
+    {
+        width_       = width;
+        height_      = height;
+        window_name_ = name;
+
+        init_window();
+    }
+
+    auto window::should_close() const -> bool
+    {
+        return glfwWindowShouldClose(window_ptr_);
+    }
+
+    void window::init_window()
+    {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // we are not using OpenGL, hence the GLFW_NO_API
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+        window_ptr_ = glfwCreateWindow(width_, height_, window_name_.c_str(), nullptr, nullptr);
+        glfwSetWindowUserPointer(window_ptr_, this);
+        glfwSetFramebufferSizeCallback(window_ptr_, framebuffer_resize_callback);
+    }
+}
---
+++ src/engine/window.h
@@ -0,0 +1,50 @@
+﻿#pragma once
+
+// Project includes
+#include "src/utility/singleton.h"
+
+// Standard includes
+#include <string>
+
+// GLFW includes
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+namespace dae
+{
+    class window final : public singleton<window>
+    {
+    public:
+        ~window() override;
+
+        window(window const &other)            = delete;
+        window(window &&other)                 = delete;
+        window &operator=(window const &other) = delete;
+        window &operator=(window &&other)      = delete;
+
+        void init(int width, int height, std::string const &name);
+
+        [[nodiscard]] auto should_close() const -> bool;
+        [[nodiscard]] auto get_extent() const -> VkExtent2D { return {static_cast<uint32_t>(width_), static_cast<uint32_t>(height_)};}
+        [[nodiscard]] auto was_window_resized() const -> bool { return frame_buffer_resized_; }
+        [[nodiscard]] auto get_glfw_window() const -> GLFWwindow* { return window_ptr_; }
+        void reset_window_resized_flag() { frame_buffer_resized_ = false; }
+        
+        void create_window_surface(VkInstance instance, VkSurfaceKHR *surface_ptr);
+
+    private:
+        friend class singleton<window>;
+        window() = default;
+
+    private:
+        static void framebuffer_resize_callback(GLFWwindow *window_ptr, int width, int height);
+        void init_window();
+
+    private:
+        GLFWwindow *window_ptr_ = nullptr;
+        int width_;
+        int height_;
+        bool frame_buffer_resized_ = false;
+        std::string window_name_;
+    };
+}
